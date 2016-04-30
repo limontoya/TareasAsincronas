d18dda990af376a221eca57265743aab5d855d39
kenosome <kennyj.osorio@gmail.com>
2016-04-30 10:41:38 -0500

    Initial commit - Threads

---
+++ app/src/main/java/montoyalinaj/tareasasincronas/MainActivity.java
@@ -0,0 +1,32 @@
+package montoyalinaj.tareasasincronas;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //Create worker thread
+    public class TareaAsincrona extends AsyncTask<Void, Void, Void>{
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+}
