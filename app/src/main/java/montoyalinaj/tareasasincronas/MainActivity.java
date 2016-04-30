f5ee78a9afd987ddfac8be274c59f3ea4093ee6d
kenosome <kennyj.osorio@gmail.com>
2016-04-30 11:13:56 -0500

    -Async task

--- app/src/main/java/montoyalinaj/tareasasincronas/MainActivity.java
+++ app/src/main/java/montoyalinaj/tareasasincronas/MainActivity.java
@@ -6,27 +6,100 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
+
+    TextView reloj;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Reference to id created on xml: R id reloj
+        reloj = (TextView) findViewById(R.id.reloj);
     }
 
-    //Create worker thread
-    public class TareaAsincrona extends AsyncTask<Void, Void, Void>{
+    public void mostrarMensaje(View view) {
+        Toast.makeText(this, "OKK", Toast.LENGTH_SHORT).show();
 
+        TareaAsincrona tarea = new TareaAsincrona();
+        tarea.execute();
+    }
+
+    /**
+     * Create worker thread
+     */
+    public class TareaAsincrona extends AsyncTask<Void, String, Void>{
+
+        /**
+         * Sets a message before excecuting the task
+         */
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            reloj.setText("Ha iniciado el proceso");
+        }
+
+        /**
+         * If something is happening on background it might be useful
+         * to use a PREexcecute and POSTexcecute methods
+         * @param params
+         * @return
+         */
         @Override
         protected Void doInBackground(Void... params) {
+            delay(1000);
+            //show message on gui
+            publishProgress("Primer segundo");
+
+            delay(1000);
+            publishProgress("Segundo segundo");
+            delay(1000);
+            publishProgress("Tercer segundo");
+            delay(1000);
+
+            return null;
+        }
+
+        /**
+         * Runs on GUI thread
+         * @param values
+         */
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+
+            String mensaje = values[0];
+
+            reloj.setText(mensaje);
+
+        }
+
+        /**
+         * Delay on the time sent
+         * @param time
+         */
+        public void delay(long time){
             try {
-                Thread.sleep(5000);
+                Thread.sleep(time);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            return null;
+        }
+
+        /**
+         * After excecuting the activity set this message
+         * @param aVoid
+         */
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            reloj.setText("Ha finalizado el proceso");
         }
     }
 }
